f1ef98e293975989cab0d05ea5156262ae9ca016
sergiolpzgmz <sergiolopezgmz@gmail.com>
2023-12-25 20:36:07 +0100

    first commit

---
+++ src/main/java/com/sergio/RaceRegistrationAPI/RaceRegistrationApiApplication.java
@@ -0,0 +1,13 @@
+package com.sergio.RaceRegistrationAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceRegistrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaceRegistrationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergio/RaceRegistrationAPI/RaceRegistrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergio.RaceRegistrationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceRegistrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
