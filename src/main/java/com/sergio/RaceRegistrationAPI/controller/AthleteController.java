2911fefb3f6db666f06af82a619e9958a91734bb
sergiolpzgmz <sergiolopezgmz@gmail.com>
2024-01-10 12:06:13 +0100

    Finished AthleteController put requests

--- src/main/java/com/sergio/RaceRegistrationAPI/controller/AthleteController.java
+++ src/main/java/com/sergio/RaceRegistrationAPI/controller/AthleteController.java
@@ -1,16 +1,15 @@
 package com.sergio.RaceRegistrationAPI.controller;
 
 import com.sergio.RaceRegistrationAPI.dto.AthleteDTO;
+import com.sergio.RaceRegistrationAPI.entity.Athlete;
+import com.sergio.RaceRegistrationAPI.exception.ApiRequestExceptionNotFound;
 import com.sergio.RaceRegistrationAPI.service.AthleteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -19,11 +18,29 @@ public class AthleteController {
     AthleteService athleteService;
 
     @PostMapping("athlete")
-    public ResponseEntity<AthleteDTO>newAthlete(@RequestBody AthleteDTO athleteDTO){
+    public ResponseEntity<AthleteDTO> newAthlete(@RequestBody AthleteDTO athleteDTO) {
         try {
             athleteService.saveAthlete(athleteDTO);
             return ResponseEntity.status(HttpStatus.CREATED).body(athleteDTO);
 
+        } catch (HttpMessageNotReadableException e) {
+            throw new HttpMessageNotReadableException(e.getMessage());
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityViolationException(e.getMessage());
+        }
+    }
+
+    @PutMapping("athlete/{id}")
+    public ResponseEntity<Athlete> updateAthlete(@RequestBody AthleteDTO athleteDTO, @PathVariable Long id) {
+        try {
+            Athlete athleteToUpdate = athleteService.findAthleteById(id);
+
+            if (athleteToUpdate == null) {
+                throw new ApiRequestExceptionNotFound("Athlete not found with id: " + id);
+            } else {
+                athleteService.updateAthlete(athleteDTO, athleteToUpdate);
+                return ResponseEntity.status(HttpStatus.CREATED).body(athleteToUpdate);
+            }
         } catch (HttpMessageNotReadableException e) {
             throw new HttpMessageNotReadableException(e.getMessage());
         } catch (DataIntegrityViolationException e) {
